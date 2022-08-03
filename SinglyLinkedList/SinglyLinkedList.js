638b1eb7df6b919cdc8971a7fe0c894f7cd4ce9b
Ravindra Patle <me24tech@gmail.com>
2022-08-03 14:25:10 +0530

    pop and unshift

--- SinglyLinkedList/SinglyLinkedList.js
+++ SinglyLinkedList/SinglyLinkedList.js
@@ -26,6 +26,47 @@ class SinglyLinkedList {
         return this;
     }
 
+    // remove a node from the end of the linked list
+    pop() {
+        if (this.length === 0) return undefined;
+
+        const poppedNode = this.tail;
+        let secondLastNode = this.getSecondLastNode();
+        this.tail = secondLastNode;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) this.head = this.tail = null;
+
+        return poppedNode;
+    }
+
+    // add a node at the start of the linked list
+    unshift(value) {
+        if (value === undefined) return 'Please provide a value';
+
+        let newNode = new Node(value);
+        if (this.length === 0) {
+            this.head = this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+
+    // helper function to get second last node from linked list
+    getSecondLastNode() {
+        let current = this.head;
+        let secondLast = current;
+        while (current.next) {
+            secondLast = current;
+            current = current.next;
+        }
+        return secondLast;
+    }
+
     print() {
         let arr = [];
         let current = this.head;
@@ -33,7 +74,8 @@ class SinglyLinkedList {
             arr.push(current.value);
             current = current.next;
         }
-        console.log(arr);
+        // console.log(arr);
+        return arr;
     }
 }
 
@@ -42,4 +84,14 @@ list.push(0);
 list.push(1);
 list.push(2);
 list.push(3);
-list.print();
+console.log('after push ', list.print());
+
+list.pop();
+list.pop();
+list.pop();
+console.log('after pop ', list.print());
+list.unshift('10');
+
+console.log('after unshift ', list.print());
+
+console.log('final list \n ', list);
