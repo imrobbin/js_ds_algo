eeb79ed55b07ca752b8f313226f4acd2cbbdc1c2
Ravindra Patle <me24tech@gmail.com>
2022-08-03 21:52:08 +0530

    Linear search

---
+++ Searching/LinearSearch.js
@@ -0,0 +1,24 @@
+// Linear Search
+// Write a function called linearSearch which accepts an array and a value,
+// and returns the index at which the value exists.
+// If the value does not exist in the array, return -1.
+// Don't use indexOf to implement this function!
+
+// Time Complexity - O(n)
+
+class LinearSearch {
+    constructor() {}
+    linearSearch(arr, element) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === element) return i;
+        }
+        return -1;
+    }
+}
+
+let linear = new LinearSearch();
+
+console.log(linear.linearSearch([53, 42, 12, 14, 20, 7, 43, 52, 46, 0], 7));
+console.log(linear.linearSearch([53, 42, 12, 14, 20, 7, 43, 52, 46, 0], 23));
+console.log(linear.linearSearch([53, 42, 12, 14, 20, 7, 43, 52, 46, 0], 20));
+console.log(linear.linearSearch([53, 42, 12, 14, 20, 7, 43, 52, 46, 0], -1));
